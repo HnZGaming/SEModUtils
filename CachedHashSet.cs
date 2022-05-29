7cf7a75e328e17cf478ef218421c00cca914fd9a
Ryoichiro Oka <ryoka0418@gmail.com>
2022-05-29 14:53:04 +0900

    cached set

---
+++ CachedHashSet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace HNZ.Utils
+{
+    public sealed class CachedHashSet<T> : IEnumerable<T>
+    {
+        readonly HashSet<T> _hashset;
+        readonly ConcurrentQueue<T> _removes;
+        readonly ConcurrentQueue<T> _adds;
+
+        public CachedHashSet()
+        {
+            _hashset = new HashSet<T>();
+            _removes = new ConcurrentQueue<T>();
+            _adds = new ConcurrentQueue<T>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            lock (this)
+            {
+                return _hashset.GetEnumerator();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void ApplyChanges()
+        {
+            lock (this)
+            {
+                _hashset.ExceptWith(_removes);
+                _hashset.UnionWith(_adds);
+                _removes.DequeueAll();
+                _adds.DequeueAll();
+            }
+        }
+
+        public void Remove(T element)
+        {
+            lock (this)
+            {
+                _removes.Enqueue(element);
+            }
+        }
+
+        public void Add(T element)
+        {
+            lock (this)
+            {
+                _adds.Enqueue(element);
+            }
+        }
+    }
+}
---
+++ SceneEntityCachedCollection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Sandbox.Game.Entities;
+using VRage.Game.Entity;
+using VRage.ModAPI;
+
+namespace HNZ.Utils
+{
+    public sealed class SceneEntityCachedCollection<T> where T : class, IMyEntity
+    {
+        readonly CachedHashSet<T> _hashset;
+
+        public SceneEntityCachedCollection()
+        {
+            _hashset = new CachedHashSet<T>();
+        }
+
+        public void Initialize()
+        {
+            MyEntities.OnEntityAdd += OnEntityAdd;
+            MyEntities.OnEntityRemove += OnEntityRemove;
+        }
+
+        public void Close()
+        {
+            MyEntities.OnEntityAdd -= OnEntityAdd;
+            MyEntities.OnEntityRemove -= OnEntityRemove;
+        }
+
+        public IEnumerable<T> ApplyChanges()
+        {
+            _hashset.ApplyChanges();
+            return _hashset;
+        }
+
+        void OnEntityAdd(MyEntity obj)
+        {
+            var t = obj as T;
+            if (t != null)
+            {
+                _hashset.Add(t);
+            }
+        }
+
+        void OnEntityRemove(MyEntity obj)
+        {
+            var t = obj as T;
+            if (t != null)
+            {
+                _hashset.Remove(t);
+            }
+        }
+    }
+}
